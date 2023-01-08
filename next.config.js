7c41c623c2bdcc37bee44d59904e1da5a6788375
Kasper Welbers <kasperwelbers@gmail.com>
2023-01-08 12:32:13 +0100

    removed custom babel so that next uses SWC

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: {},
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
